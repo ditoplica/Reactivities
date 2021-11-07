d43d49bb378827a15f3c78f097f6ac07c57e2fb0
Endrit Toplica <endrit.toplica@asseco-see.com>
2021-11-07 10:12:54 +0100

    End of section 4

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -2,7 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence;
 
 namespace API.Controllers
 {
@@ -10,6 +13,9 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class BaseApiController : ControllerBase
     {
-        
+        private IMediator _mediator;
+
+        // ??= --> null coalescing assignment operator
+        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();                  
     }
 }
